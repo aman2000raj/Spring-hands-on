0f5c963759261d2a545c209157218efea257ea1a
aman2000raj <amankayshap10@gmail.com>
2024-04-29 17:40:28 +0530

    updates ExceptionHandler to handle more than one exception

--- src/main/java/org/aman/exceptions/AppExceptionsHandler.java
+++ src/main/java/org/aman/exceptions/AppExceptionsHandler.java
@@ -1,17 +1,52 @@
 package org.aman.exceptions;
 
+import org.aman.ui.model.response.ErrorMessage;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.context.request.WebRequest;
 
+import java.util.Date;
+
 
 public class AppExceptionsHandler {
     @ExceptionHandler(value = {Exception.class})
     public ResponseEntity<Object> handleAnyException(Exception ex, WebRequest request){
+
+        String errorMessageDescription = ex.getLocalizedMessage();
+        if(errorMessageDescription == null) errorMessageDescription = ex.toString();
+
+        ErrorMessage errorMessage = new ErrorMessage(new Date(), errorMessageDescription);
+
         return new ResponseEntity<>(
                 ex, new HttpHeaders(), HttpStatus.INTERNAL_SERVER_ERROR
         );
     }
+
+    @ExceptionHandler(value = {NullPointerException.class, UserServiceException.class})
+    public ResponseEntity<Object> handleNullException(Exception ex, WebRequest request){
+
+        String errorMessageDescription = ex.getLocalizedMessage();
+        if(errorMessageDescription == null) errorMessageDescription = ex.toString();
+
+        ErrorMessage errorMessage = new ErrorMessage(new Date(), errorMessageDescription);
+
+        return new ResponseEntity<>(
+                ex, new HttpHeaders(), HttpStatus.INTERNAL_SERVER_ERROR
+        );
+    }
+
+//    @ExceptionHandler(value = {UserServiceException.class})
+//    public ResponseEntity<Object> handleUserServiceException(UserServiceException ex, WebRequest request){
+//
+//        String errorMessageDescription = ex.getLocalizedMessage();
+//        if(errorMessageDescription == null) errorMessageDescription = ex.toString();
+//
+//        ErrorMessage errorMessage = new ErrorMessage(new Date(), errorMessageDescription);
+//
+//        return new ResponseEntity<>(
+//                ex, new HttpHeaders(), HttpStatus.INTERNAL_SERVER_ERROR
+//        );
+//    }
 }
