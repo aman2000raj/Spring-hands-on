35f678ac03b2f823ff042f7b8cc87ac1e64aac4a
aman2000raj <amankayshap10@gmail.com>
2024-04-29 18:52:43 +0530

    (update) adds dependency injection for create User

--- src/main/java/org/aman/ui/controller/UserController.java
+++ src/main/java/org/aman/ui/controller/UserController.java
@@ -1,2 +1,91 @@
-package org.aman.ui.controller;public class UserController {
+package org.aman.ui.controller;
+
+import jakarta.validation.Valid;
+import org.aman.exceptions.UserServiceException;
+import org.aman.ui.model.request.UpdateUserDetailsRequestModel;
+import org.aman.ui.model.request.UserDetailsRequestModel;
+import org.aman.ui.model.response.UserRest;
+import org.aman.userservice.implementor.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.UUID;
+
+@RestController
+
+@RequestMapping("users") // http://localhost:8080/users
+public class UserController {
+
+    Map<String, UserRest> users;
+
+    @Autowired
+    UserService userService;
+
+    @GetMapping
+    public String getUsers(@RequestParam(value="page", defaultValue = "1") int page,
+                           @RequestParam(value = "limit", defaultValue = "50") int limit,
+                           @RequestParam(value = "sort", defaultValue = "desc",required = false) String sort){
+        return "get users was called with page " + page + " and limit " + limit + " and sort " + sort;
+    }
+
+    @GetMapping(path = "/{userId}",
+            produces = {
+                    MediaType.APPLICATION_XML_VALUE,
+                    MediaType.APPLICATION_JSON_VALUE
+            })
+    public ResponseEntity<UserRest> getUsers(@PathVariable String userId) {
+
+        //if(true) throw new UserServiceException("A user service exception is thrown.");
+
+        if(users.containsKey(userId)) {
+            return new ResponseEntity<>(users.get(userId), HttpStatus.OK);
+        }
+        return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+    }
+
+    @PostMapping(
+            consumes = {
+                    MediaType.APPLICATION_XML_VALUE,
+                    MediaType.APPLICATION_JSON_VALUE
+            },
+            produces = {
+                    MediaType.APPLICATION_XML_VALUE,
+                    MediaType.APPLICATION_JSON_VALUE
+            })
+    public ResponseEntity<UserRest> createUser(@Valid @RequestBody UserDetailsRequestModel userDetails) {
+        UserRest returnValue = userService.CreateUser(userDetails);
+        return new ResponseEntity<UserRest>(returnValue, HttpStatus.OK);
+    }
+
+    @PutMapping(path = "/{userId}",
+            consumes = {
+                    MediaType.APPLICATION_XML_VALUE,
+                    MediaType.APPLICATION_JSON_VALUE
+            },
+            produces = {
+                    MediaType.APPLICATION_XML_VALUE,
+                    MediaType.APPLICATION_JSON_VALUE
+            }
+    )
+    public UserRest updateUser(@PathVariable String userId, @Valid @RequestBody UpdateUserDetailsRequestModel userDetails) {
+        UserRest storedUserDetails = users.get(userId);
+        storedUserDetails.setFirstName(userDetails.getFirstName());
+        storedUserDetails.setLastName(userDetails.getLastName());
+
+        users.put(userId, storedUserDetails);
+        return storedUserDetails;
+    }
+
+    @DeleteMapping(path = "/{id}")
+    public ResponseEntity<Void> deleteUser(@PathVariable String id) {
+        users.remove(id);
+        return ResponseEntity.noContent().build();
+    }
+
 }
+
