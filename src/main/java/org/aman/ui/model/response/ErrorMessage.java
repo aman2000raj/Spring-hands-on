0b662ba69ebb5863b29de9757c48442ed5ccdb87
aman2000raj <amankayshap10@gmail.com>
2024-04-29 17:47:29 +0530

    update exception for coustom messages

--- src/main/java/org/aman/ui/model/response/ErrorMessage.java
+++ src/main/java/org/aman/ui/model/response/ErrorMessage.java
@@ -1,2 +1,30 @@
-package org.aman.ui.model.response;public class ErrorMessage {
+package org.aman.ui.model.response;
+
+import java.util.Date;
+
+public class ErrorMessage {
+    private Date timestamp;
+    private String message;
+
+    public ErrorMessage() {}
+
+    public ErrorMessage(Date timestamp, String message) {
+        this.timestamp = timestamp;
+        this.message = message;
+    }
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public Date getTimestamp() {
+        return timestamp;
+    }
+
+    public void setTimestamp(Date timestamp) {
+        this.timestamp = timestamp;
+    }
 }
